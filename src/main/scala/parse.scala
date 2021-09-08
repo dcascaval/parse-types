e7546883b7657766643566a0de3abfc77e3c733f
Dan Cascaval <dan@cascaval.us>
2021-09-08 02:45:36 -0700

    wip: most top-level types

--- src/main/scala/parse.scala
+++ src/main/scala/parse.scala
@@ -1,5 +1,7 @@
 package tsparse
 
+import scala.collection.mutable.ArrayBuffer
+
 import fastparse._
 import JavaWhitespace._ // Ignore whitespace and //, /* */ comment blocks
 import fastparse.Parsed.Success
@@ -7,28 +9,15 @@ import fastparse.Parsed.Failure
 
 object Parser {
 
-  // KEYWORDS
-  def cls[_: P] = P("class")
-  def itf[_: P] = P("interface")
-  def typ[_: P] = P("type")
-  def exp[_: P] = P("export")
-  def enm[_: P] = P("enum")
-  def stc[_: P] = P("static")
-  def rdo[_: P] = P("readonly")
-
   // SYNTAX
   def colon[_: P] = P(":")
   def comma[_: P] = P(",")
   def braced[_: P, T](p: => P[T]) = P("{" ~ p ~ "}")
   def withParens[_: P, T](p: => P[T]) = P("(" ~ p ~ ")")
 
-  def colonQ[_: P]: P[Boolean] = P((":" | "?:").!).map(s => s.length() == 2)
-
   def identColonQ[_: P]: P[(String, Boolean)] = P(
     ident ~ "?".?.! ~ ":"
-  ).map { case (name, q) =>
-    (name, q.length() > 0)
-  }
+  ).map { case (name: String, q: String) => (name, q.length() > 0) }
 
   // IDENTIFIERS
   def alphaUnder[_: P] = P(CharPred(c => c.isLetter || c == '_'))
@@ -36,17 +25,18 @@ object Parser {
   def ident[_: P] = P((alphaUnder ~ alphaUnderDigit.?).!)
 
   // PARAMETERS
-  def parameter[_: P]: P[Argument] = P(identColonQ ~ dataType).map { case (name, rdo, data) =>
-    Argument(name, data, rdo)
-  }
+  def parameter[_: P]: P[Argument] =
+    P(identColonQ ~ dataType).map { case (name: String, optional: Boolean, data: DataType) =>
+      Argument(name, data, optional)
+    }
   def argumentList[_: P]: P[Seq[Argument]] = P("(" ~ parameter.rep(0, sep = ",") ~ ",".? ~ ")")
 
+  //
   // TYPES
   //
-
   // Consume array suffixes after any successful type parse so we don't infinitely recurse
   def withArray[_: P](baseParser: => P[DataType]): P[DataType] =
-    P(baseParser ~ "[]".!.rep(0)).map { case (baseType, seq) =>
+    P(baseParser ~ "[]".!.rep(0)).map { case (baseType: DataType, seq: Seq[String]) =>
       seq.foldLeft[DataType](baseType)((t, _) => ArrayType(t))
     }
 
@@ -57,28 +47,47 @@ object Parser {
   // Curve<A, B>
   def parameterizedType[_: P]: P[DataType] = P(
     alphaUnderDigit.! ~ "<" ~/ dataType.rep(1, sep = ",") ~ ">"
-  ).map { case (name, ps) => Parameterized(name, ps) }
+  ).map { case (name: String, ps: Seq[DataType]) => Parameterized(name, ps) }
 
   // A | B | C
   def unionType[_: P]: P[DataType] = withArray(
     P(
-      nonUnionType.rep(2, sep = "|"./)
+      "|".? ~ nonUnionType.rep(2, sep = "|"./)
     ).map(mems => UnionType(Set.from(mems)))
   )
 
   // arrow (err: Error, data: Uint8Array, final: boolean) => void;
   def arrowType[_: P]: P[DataType] = P(
     argumentList ~/ "=>" ~ dataType
-  ).map { case (ps, ret) => ArrowType(ps, ret) }
+  ).map { case (args: Seq[Argument], ret: DataType) => ArrowType(args, ret) }
   // [ A , B ]
   def tupleType[_: P]: P[DataType] = P(
     "[" ~/ dataType.rep(1, sep = ",") ~ "]"
   ).map(TupleType(_))
 
+  def interfaceParameter[_: P] = P(parameter ~ ";")
+  def interfaceKey[_: P] = P("[" ~/ ident ~ ":" ~ dataType ~ "]" ~ ":" ~ dataType ~ ";").map {
+    case (name, keyType, returnType) => Key(name, keyType, returnType)
+  }
+
+  def objectType[_: P]: P[DataType] = P(
+    "{" ~/ (interfaceParameter | interfaceKey).rep(0) ~ "}"
+  ).map(mems => {
+    val args = new ArrayBuffer[Argument]()
+    val keys = new ArrayBuffer[Key]()
+    for (mem <- mems) {
+      mem match {
+        case a: Argument => args += a
+        case k: Key      => keys += k
+      }
+    }
+    ObjectType(args.toSeq, keys.toSeq)
+  })
+
   def nonUnionType[_: P]: P[DataType] = withArray(
     P(
       arrowType | parameterizedType | baseType |
-        tupleType | stringType
+        tupleType | stringType | objectType
     )
   )
 
@@ -87,20 +96,223 @@ object Parser {
     NoCut(unionType) | nonUnionType | withArray("(" ~ dataType ~ ")")
   )
 
-  def withEnd[_: P, T](p: => P[T]) = P(p ~ End)
+  // <Foo extends A = B>
+  def typeArgument[_: P]: P[TypeParameterDecl] = P(
+    ident ~ ("extends" ~/ dataType).? ~ ("=" ~/ dataType).?
+  ).map { case (name: String, ext: Option[DataType], default: Option[DataType]) =>
+    TypeParameterDecl(name, ext, default)
+  }
 
-  //
+  def typeArgumentList[_: P] = P("<" ~/ typeArgument.rep(1, sep = ",") ~ ">")
+
+  def extensionClause[_: P]: P[Seq[DataType]] = P("extends" ~/ dataType.rep(1, sep = ","))
+  def implementsClause[_: P]: P[DataType] = P("implements" ~/ dataType)
+
+  // MEMBERS
+  def getSet[_: P] = P(("get" | "set").!).map(str => if (str == "get") Getter else Setter)
+  def valueMember[_: P] = P("readonly".!.? ~ identColonQ ~ dataType ~ ";").map {
+    case (readOnly, (name, optional), typ) =>
+      ValueMember(name, typ, optional, readOnly.isDefined)
+  }
+  def staticMember[_: P] = ("static" ~/ "readonly".!.? ~ ident ~ dataType ~ ";").map {
+    case (readOnly, name, typ) =>
+      StaticMember(name, typ, readOnly.isDefined) // never optional
+  }
+  def functionMember[_: P] = P(getSet.? ~ ident ~ argumentList ~/ ":" ~ dataType ~ ";").map {
+    case (gs, name, args, ret) =>
+      FnMember(name, args, ret, gs)
+  }
+  def constructor[_: P] = P("constructor" ~/ argumentList ~ ";").map(Constructor(_))
+  def classMember[_: P] = P(functionMember | staticMember | valueMember | constructor)
 
   // STATEMENTS
   def importStmt[_: P] = P("import" ~ CharsWhile(_ != ';') ~ ";")
 
-  def topLevel[_: P]: P[TopLevelStatement] = ???
-  def run[_: P] = P(topLevel.rep(0) ~ End)
+  def topConstant[_: P] = P("export" ~ "const" ~/ ident ~ ":" ~ dataType ~ ";").map { case (name, typ) =>
+    TopLevelConstant(name, typ)
+  }
+
+  def topInterface[_: P] = P(
+    "export" ~ "interface" ~/ ident ~ typeArgumentList.? ~ extensionClause.? ~ "{" ~
+      valueMember.rep(0) ~ "}"
+  ).map { case (name, args, exts, members) =>
+    new Interface(name, args, members, exts)
+  }
+
+  def topClass[_: P] = P(
+    "export" ~ "class" ~/ ident ~ typeArgumentList.? ~ extensionClause.? ~ implementsClause.? ~ "{" ~
+      classMember.rep(0) ~ "}"
+  ).map { case (name, typeArgs, extensions, implements, members) =>
+    val values = ArrayBuffer[ValueMember]()
+    val statics = ArrayBuffer[StaticMember]()
+    val functions = ArrayBuffer[FnMember]()
+    val ctrs = ArrayBuffer[Constructor]()
+    for (mem <- members) {
+      mem match {
+        case v: ValueMember  => values += v
+        case s: StaticMember => statics += s
+        case f: FnMember     => functions += f
+        case c: Constructor  => ctrs += c
+      }
+    }
+
+    new BasicClass(
+      name,
+      typeArgs,
+      values.toSeq,
+      statics.toSeq,
+      functions.toSeq,
+      ctrs.toSeq,
+      extensions,
+      implements
+    )
+  }
+
+  def topType[_: P] = P(
+    "export" ~ "type" ~/ ident ~ "=" ~ dataType ~ ";"
+  ).map { case (name, typ) => TopLevelType(name, typ) }
+
+  def digits[_: P] = P(CharsWhile(_.isDigit).!).map(_.toInt)
+
+  def enumString[_: P] = P("'" ~/ ident ~ "'").map(StringMem(_))
+  def enumValue[_: P] = P(ident ~ "=" ~/ digits).map { case (name, value) => ValueMem(name, value) }
+  def enumBasic[_: P] = P(ident).map(BasicMember(_))
+
+  def enumMember[_: P] = P(enumValue | enumString | enumBasic)
+
+  def topEnum[_: P] = P(
+    "export" ~ "enum" ~/ ident ~ "{" ~ enumMember.rep(0) ~ "}"
+  ).map { case (name, mems) =>
+    TopLevelEnum(name, mems)
+  }
+  //
+  //
+  //
+  def withEnd[_: P, T](p: => P[T]) = P(p ~ End)
+
+  def topLevelStmt[_: P]: P[TopLevelStatement] = (
+    topEnum | topInterface | topClass | topConstant | topType
+  )
+
+  def allTop[_: P]: P[Seq[TopLevelStatement]] = (
+    (importStmt | topLevelStmt)
+      .rep(0)
+    )
+    .map(stmts =>
+      stmts.flatMap(s =>
+        s match {
+          case impt: Unit              => None
+          case stmt: TopLevelStatement => Some(stmt)
+        }
+      )
+    )
+
+  def run[_: P] = P(topLevelStmt.rep(0) ~ End)
 }
 
 object TestParse extends App {
   import Parser._
+  import TestHelpers._
 
+  BaseTests.run
+
+  withPrint {
+    parseWhole(
+      """export interface MorphTarget {
+      name: string;
+      vertices: Vector3[];
+  }""",
+      topInterface(_)
+    )
+  }
+
+  println("Success")
+
+}
+
+// TODO:
+//   - type parameters for arrow
+//   - varargs
+//   - optional interface function types???
+//   - special casing
+
+object BaseTests {
+  import Parser._
+  import TestHelpers._
+
+  def run() = {
+    val exImport = "import { BufferAttribute } from './../core/BufferAttribute';"
+    parseWhole(exImport, importStmt(_))
+
+    info("idents")
+    parseWhole("fff", ident(_))
+    parseWhole("fff", baseType(_))
+    parseWhole("fff", dataType(_))
+
+    info("arrays")
+    parseWhole("number[]", dataType(_))
+    parseWhole("number[][]", dataType(_))
+    parseWhole("(number)[][]", dataType(_))
+
+    info("parens")
+    parseWhole("(fff)", dataType(_))
+
+    info("unions")
+    parseWhole("number | undefined", unionType(_))
+    parseWhole("number | undefined | string | what", unionType(_))
+    parseWhole("number | undefined", dataType(_))
+    parseWhole("number | undefined | string | what", dataType(_))
+
+    info("parameters")
+    parseWhole("k: number", parameter(_))
+
+    info("arrows")
+    parseWhole("() => void", arrowType(_))
+    parseWhole("(k: number) => void", arrowType(_))
+    parseWhole("(i: k, j?: q) => fff", arrowType(_))
+    parseWhole("(kk: k[], jolly_roger?: q) => fff", arrowType(_))
+    parseWhole("(kk: No[], jolly_roger?: Yes) => fff", dataType(_))
+    parseWhole("() => void", dataType(_))
+
+    info("paramTypes")
+    parseWhole("Curve<K>", dataType(_))
+    parseWhole("Curve<K,Z[]>", dataType(_))
+    parseWhole("A<B<C>>", dataType(_))
+    parseWhole("K<L<M>,N|Z,()=>O>", dataType(_))
+
+    info("constArrays")
+    parseWhole("[number, number]", dataType(_))
+    parseWhole("[number, K<F>[][]]", dataType(_))
+    parseWhole("[[number], o[][]]", dataType(_))
+
+    info("objects")
+    parseWhole("{}", dataType(_))
+    parseWhole("{ uniform: bool; test?: case; }", dataType(_))
+    parseWhole("{ uniform: { DISTANCE: {}; }; test?: case; }", dataType(_))
+    parseWhole("{ [key: number]: IUniform; test?: case; }", dataType(_))
+
+    info("integration")
+    val tts = Seq(
+      "number | undefined",
+      "(number | undefined)[]",
+      "number[] | undefined",
+      "'foo' | 'bar'",
+      """(segment: LightningSegment[],
+                 parentSubray: () => Lightning[],
+                 childSubray?: (ff: K) => 'string' | void,
+                 lightningStrike: LightningStrike,
+             ) => void"""
+    )
+
+    def dtEnd[_: P] = withEnd(dataType)
+
+    for (tt <- tts) {
+      parseWhole(tt, dtEnd(_))
+    }
+  }
+}
+
+object TestHelpers {
   var print = false
   def withPrint(f: => Any) = {
     print = true
@@ -126,70 +338,4 @@ object TestParse extends App {
 
   def info(str: String) =
     println(s"\n> ${str.toUpperCase()} ${"-" * (100 - str.length())} \n")
-
-  val exImport = "import { BufferAttribute } from './../core/BufferAttribute';"
-  parseWhole(exImport, importStmt(_))
-
-  info("idents")
-  parseWhole("fff", ident(_))
-  parseWhole("fff", baseType(_))
-  parseWhole("fff", dataType(_))
-
-  info("arrays")
-  parseWhole("number[]", dataType(_))
-  parseWhole("number[][]", dataType(_))
-  parseWhole("(number)[][]", dataType(_))
-
-  info("parens")
-  parseWhole("(fff)", dataType(_))
-
-  info("unions")
-  parseWhole("number | undefined", unionType(_))
-  parseWhole("number | undefined | string | what", unionType(_))
-  parseWhole("number | undefined", dataType(_))
-  parseWhole("number | undefined | string | what", dataType(_))
-
-  info("parameters")
-  parseWhole("k: number", parameter(_))
-
-  info("arrows")
-  parseWhole("() => void", arrowType(_))
-  parseWhole("(k: number) => void", arrowType(_))
-  parseWhole("(i: k, j?: q) => fff", arrowType(_))
-  parseWhole("(kk: k[], jolly_roger?: q) => fff", arrowType(_))
-  parseWhole("(kk: No[], jolly_roger?: Yes) => fff", dataType(_))
-  parseWhole("() => void", dataType(_))
-
-  info("paramTypes")
-  parseWhole("Curve<K>", dataType(_))
-  parseWhole("Curve<K,Z[]>", dataType(_))
-  parseWhole("A<B<C>>", dataType(_))
-  parseWhole("K<L<M>,N|Z,()=>O>", dataType(_))
-
-  info("constArrays")
-  parseWhole("[number, number]", dataType(_))
-  parseWhole("[number, K<F>[][]]", dataType(_))
-  parseWhole("[[number], o[][]]", dataType(_))
-
-  info("integration")
-  val tts = Seq(
-    "number | undefined",
-    "(number | undefined)[]",
-    "number[] | undefined",
-    "'foo' | 'bar'",
-    """(segment: LightningSegment[],
-              parentSubray: () => Lightning[],
-              childSubray?: (ff: K) => 'string' | void,
-              lightningStrike: LightningStrike,
-          ) => void"""
-  )
-
-  def dtEnd[_: P] = withEnd(dataType)
-
-  for (tt <- tts) {
-    parseWhole(tt, dtEnd(_))
-  }
-
-  println("Success")
-
 }
--- src/main/scala/types.scala
+++ src/main/scala/types.scala
@@ -7,39 +7,69 @@ sealed trait DataType
 case class Base(name: String) extends DataType // number, undefined, void, etc.
 case class StringType(value: String) extends DataType // 'foo'
 case class Parameterized(name: String, parameters: Seq[DataType]) extends DataType // ArrayLike<number>
-case class UnionType(members: Set[DataType]) extends DataType // number | undefined
 case class ArrayType(member: DataType) extends DataType // number[]
 case class ArrowType(parameters: Seq[Argument], ret: DataType) extends DataType
 case class TupleType(members: Seq[DataType]) extends DataType // [number, number, number]
+// TODO: remove undefined from this set if it's optional on an arg/parameter
+case class UnionType(members: Set[DataType]) extends DataType // number | undefined
+case class ObjectType(members: Seq[Argument], keys: Seq[Key]) extends DataType
 
 case class Argument(name: String, dataType: DataType, optional: Boolean)
+case class Key(name: String, dataType: DataType, returnType: DataType)
+
+sealed trait GetSetState
+case object Getter extends GetSetState
+case object Setter extends GetSetState
+
+sealed trait Member
+
 // [readonly] foo: A;
-case class Member(name: String, dataType: DataType, optional: Boolean = false, readOnly: Boolean = false)
+case class ValueMember(
+    name: String,
+    dataType: DataType,
+    optional: Boolean,
+    readOnly: Boolean
+) extends Member
+
 // static foo: A;
-case class StaticMember(name: String, datatype: DataType) // Goes in companion object
+case class StaticMember(name: String, datatype: DataType, readOnly: Boolean)
+    extends Member // Goes in companion object
+
 // setFoo(foo: A, bar?: B);
-case class FnMember(name: String, parameters: Seq[Member])
+case class FnMember(
+    name: String,
+    parameters: Seq[Argument],
+    returnType: DataType,
+    getSet: Option[GetSetState]
+) extends Member
+
+case class Constructor(
+    parameters: Seq[Argument]
+) extends Member
 
 // In scala there isn't such a thing as a default type parameter, so we
 // will instead remember the default and replace it at all invocation sites.
-case class TypeParameterDecl(name: String, default: Option[DataType])
+case class TypeParameterDecl(name: String, extension: Option[DataType], default: Option[DataType])
 
 sealed trait TopLevelStatement
 
 // export const foo: A;
-class TopLevelConstant(
+case class TopLevelConstant(
     name: String,
-    dataType: String
+    dataType: DataType
 ) extends TopLevelStatement
 
-// export enum Foo {
-//  A = 0,
-//  B = "x",
-//  C
-// }
-class TopLevelEnum(
+sealed trait EnumMember
+case class StringMem(value: String) extends EnumMember
+case class ValueMem(name: String, value: Int) extends EnumMember
+case class BasicMember(name: String) extends EnumMember
+
+// Ex:
+// export enum Foo { A = 0, BAR, }
+// export enum Baz { 'foo', 'bar' }
+case class TopLevelEnum(
     name: String,
-    options: Seq[(String, Option[String])]
+    options: Seq[EnumMember]
 ) extends TopLevelStatement
 
 // export class Foo extends Bar {
@@ -49,16 +79,17 @@ class TopLevelEnum(
 // }
 class BasicClass(
     name: String,
-    typeParameters: Seq[String],
-    members: Seq[Member],
+    typeParameters: Option[Seq[TypeParameterDecl]],
+    values: Seq[ValueMember],
+    statics: Seq[StaticMember],
     functions: Seq[FnMember],
-    constructors: Seq[FnMember],
-    extensions: Seq[String],
-    implements: Seq[String] // class Foo implements IFoo {}
+    constructors: Seq[Constructor],
+    extensions: Option[Seq[DataType]],
+    implements: Option[DataType] // class Foo implements IFoo {}
 ) extends TopLevelStatement
 
 // export type Foo = [number, number] | string;
-class TopLevelType(name: String, dataType: DataType) extends TopLevelStatement
+case class TopLevelType(name: String, dataType: DataType) extends TopLevelStatement
 
 //
 // export interface Foo<T = any> {
@@ -66,9 +97,9 @@ class TopLevelType(name: String, dataType: DataType) extends TopLevelStatement
 //   baz: T;
 // }
 //
-class Interface(
+case class Interface(
     name: String,
-    parameters: Seq[TypeParameterDecl],
-    members: Seq[Member],
-    extensions: Seq[String]
+    parameters: Option[Seq[TypeParameterDecl]],
+    members: Seq[ValueMember],
+    extensions: Option[Seq[DataType]]
 ) extends TopLevelStatement
